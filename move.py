1dcd69c01e6cef86788ef58e5b7f335d9ebb3051
rosxar <157671754+rosxar@users.noreply.github.com>
2024-01-31 16:13:26 +0700

    Create move.py

---
+++ move.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+def move_files_to_main_folder(root_folder):
+    # Loop through subfolders
+    for foldername, subfolders, filenames in os.walk(root_folder):
+        # Skip the root folder to avoid moving files from the main folder
+        if foldername == root_folder:
+            continue
+
+        # Move each file to the main folder
+        for filename in filenames:
+            source_path = os.path.join(foldername, filename)
+            destination_path = os.path.join(root_folder, filename)
+
+            # Check if the file already exists in the main folder
+            if not os.path.exists(destination_path):
+                shutil.move(source_path, destination_path)
+                print(f"Moved: {filename} from {foldername} to {root_folder}")
+            else:
+                print(f"File {filename} already exists in {root_folder}. Skipping.")
+
+if __name__ == "__main__":
+    # Ganti path_folder_utama dengan path folder utama Anda
+    path_folder_utama = 'D:\\Bisnis\\TES IMGBB\\downloaded_images'
+    move_files_to_main_folder(path_folder_utama)
